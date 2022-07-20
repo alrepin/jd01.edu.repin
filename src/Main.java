cea339d996f72cb9467744aeff448c9185091643
alrepin <alrepin@list.ru>
2022-07-21 01:41:02 +0300

    hw02 - на проверку 01

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+//import hw02.hw02;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Инициализация проекта");
+        hw02.hw02.task1();
+        hw02.hw02.task2();
+        hw02.hw02.task3();
+        hw02.hw02.task4();
+        hw02.hw02.task5();
     }
 }
