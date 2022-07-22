e9a90b5df41abfd32463cc55da4c96c7e74aac92
alrepin <alrepin@list.ru>
2022-07-22 11:24:18 +0300

    hw01 - зачтено

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 //import hw01.hw01;
 public class Main {
     public static void main(String[] args) {
-        hw01.hw01.task01();
+        //Домашнее задание по уроку "Установка инструментов разработчика" зачтено:
+        //https://my.sky.pro/student-cabinet/stream-lesson/16819/homework
+        /*hw01.hw01.task01();
+        */
     }
 }
