4ac8a4d59bacaf5b17b569d1b79fad41e5336253
alrepin <alrepin@list.ru>
2022-07-22 14:44:10 +0300

    hw03 - зачтено

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@
 
 public class Main {
     public static void main(String[] args) {
+        //Домашнее задание по уроку "Условные операторы" зачтено:
+        //https://my.sky.pro/student-cabinet/stream-lesson/16821/homework
+        /*
         hw03.hw03.info();
         hw03.hw03.task1();
         hw03.hw03.task2();
         hw03.hw03.task3();
         hw03.hw03.task4();
         hw03.hw03.task5();
-
+         */
     }
 }
