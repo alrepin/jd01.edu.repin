97fe21087cc928ce66d6ee7c393a92c97e59847c
alrepin <alrepin@list.ru>
2022-07-23 23:36:53 +0300

    hw04 - зачтено

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 import hw04.hw04;
 public class Main {
     public static void main(String[] args) {
-        //Домашнее задание по уроку "Циклы":
-        hw04.info();
-        hw04.task1();
-        hw04.task2();
-        hw04.task3();
-        //https://my.sky.pro/student-cabinet/stream-lesson/
+        //Домашнее задание по уроку "Циклы" зачтено:
+        //https://my.sky.pro/student-cabinet/stream-lesson/16822/homework
         /*
         hw04.info();
         hw04.task1();
