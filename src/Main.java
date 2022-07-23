8ff245558b2fc3f77f3076f1fc34783eca989136
alrepin <alrepin@list.ru>
2022-07-23 11:08:24 +0300

    hw template

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import course00.topic00.lesson.Lesson00;
 public class Main {
+    public static void hwCaption(String caption, boolean isReady, int linkNum) {
+        String strIsReady = "";
+        if (isReady) {
+            strIsReady = "зачтено";
+        }
+        System.out.println("\nДомашнее задание по уроку \"" + caption + "\" " + strIsReady);
+        System.out.println("https://my.sky.pro/student-cabinet/stream-lesson/" + linkNum + "/homework");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Инициализация проекта");
+        hwCaption("", false, 168);
+        course00.topic00.hw.Hw00.main();
+        course00.topic00.lesson.Lesson00.main();
     }
 }
