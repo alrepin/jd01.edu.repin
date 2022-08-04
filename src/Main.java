01a2621956d448514a81381380c76d383b2d5735
alrepin <alrepin@list.ru>
2022-08-05 02:39:37 +0300

    hw07 - зачтено

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import hw07.Hw07;
 public class Main {
     public static void main(String[] args) {
-        //Домашнее задание по уроку "Строки":
+        //Домашнее задание по уроку "Строки" зачтено:
         //https://my.sky.pro/student-cabinet/stream-lesson/16825/homework
         Hw07.info();
         Hw07.task1();
