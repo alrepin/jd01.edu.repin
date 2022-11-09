b2d364fa86b2f8e562633fb005a5313229c144ae
alrepin <alrepin@list.ru>
2022-11-10 02:00:09 +0300

    pom-edited

--- src/test/java/ga/repin/education/course02/topic16/hw/IntegerList/SortMethodsTest.java
+++ src/test/java/ga/repin/education/course02/topic16/hw/IntegerList/SortMethodsTest.java
@@ -2,27 +2,33 @@ package ga.repin.education.course02.topic16.hw.IntegerList;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
+
 import static ga.repin.education.common.UsefulMethods.generateRandomIntegerArray;
 import static ga.repin.education.course02.topic16.hw.IntegerList.SortMethods.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled("Disabled until change in sort methods")
 class SortMethodsTest {
     private final static Integer[] RND_ARR = generateRandomIntegerArray(100_000);
 
     private final static long TEST_TIMEOUT = 5555;
+
+
     @Test
-    @Disabled
+    @Disabled("enough passed ones")
     public void checkTimeMeasurement() throws InterruptedException {
         markTheTime(false);
         TimeUnit.MILLISECONDS.sleep(TEST_TIMEOUT);
         double actual = markTheTime(true);
         actual *= 1000;
-        assertEquals(TEST_TIMEOUT,actual);
+        assertEquals(TEST_TIMEOUT, actual);
     }
 
     @Test
-    @Disabled
+    @Disabled("enough passed ones")
     public void checkSortMethodsResults() {
         Integer[] actual;
 
@@ -30,23 +36,23 @@ class SortMethodsTest {
 
         actual = insertionSortMethod(RND_ARR);
         assertTrue(isArrSorted(actual));
-        assertEquals(RND_ARR.length,actual.length);
-        assertEquals(sum(RND_ARR),sum(actual));
+        assertEquals(RND_ARR.length, actual.length);
+        assertEquals(sum(RND_ARR), sum(actual));
 
         actual = quickSort(RND_ARR, 0, RND_ARR.length - 1);
         assertTrue(isArrSorted(actual));
-        assertEquals(RND_ARR.length,actual.length);
-        assertEquals(sum(RND_ARR),sum(actual));
+        assertEquals(RND_ARR.length, actual.length);
+        assertEquals(sum(RND_ARR), sum(actual));
 
         actual = selectionSortMethod(RND_ARR);
         assertTrue(isArrSorted(actual));
-        assertEquals(RND_ARR.length,actual.length);
-        assertEquals(sum(RND_ARR),sum(actual));
+        assertEquals(RND_ARR.length, actual.length);
+        assertEquals(sum(RND_ARR), sum(actual));
 
         actual = bubbleSortMethod(RND_ARR);
         assertTrue(isArrSorted(actual));
-        assertEquals(RND_ARR.length,actual.length);
-        assertEquals(sum(RND_ARR),sum(actual));
+        assertEquals(RND_ARR.length, actual.length);
+        assertEquals(sum(RND_ARR), sum(actual));
 
     }
 
@@ -60,10 +66,6 @@ class SortMethodsTest {
     }
 
     public double sum(Integer[] arr) {
-        double result = 0;
-        for (int i = 0; i < arr.length; i++) {
-            result += arr[i];
-        }
-        return result;
+        return Arrays.stream(arr).mapToDouble(integer -> integer).sum();
     }
 }
