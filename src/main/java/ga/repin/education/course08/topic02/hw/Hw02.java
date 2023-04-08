3408bbe3cf5f7b42d64d9d281493a70a522f1e99
alrepin <alrepin@list.ru>
2023-04-08 22:54:04 +0300

    ticket 3: task resolved with lang3 lib

---
+++ src/main/java/ga/repin/education/course08/topic02/hw/Hw02.java
@@ -0,0 +1,16 @@
+package ga.repin.education.course08.topic02.hw;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.Arrays;
+
+public class Hw02 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        ArrayUtils.reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+    
+}
+
